8f7823a05b91e72b2d496fa02c3a0e921b134718
FLWryoko <11754267+FLWryoko@users.noreply.github.com>
2021-10-25 02:50:41 +0900

    first commit

---
+++ quiz.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double eng,ans;
+    cout << "この素粒子な~んだクイズです" << endl;
+    cout << "出題するエネルギーの範囲を決めます:1>=0の範囲で数値を入力してください" << endl;
+    cin >> eng;
+    cout << "ありがとね!!問題出せるよ('v'*)" << endl;
+    cout << "Sはスピン量子数、S_mは磁気量子数";
+
+    if (eng == 0){
+        cout << "(A{S=1/2,S_m=1/2), (B{S=1/2,S_m=-1/2})";
+        cin >> ans;
+    }
+    
+    
+}
